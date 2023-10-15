ce2ef949df29693207a8d0b7c5418180490a8b54
Nassir Al-Khishman <khishmann@gmail.com>
2023-10-15 15:59:32 -0400

    (backend) Add 'item' data interfaces

--- backend/src/cheap/main.py
+++ backend/src/cheap/main.py
@@ -1,6 +1,28 @@
 import datetime
+from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: str
+    description: str
+
+
+class ItemsTable(Enum):
+    """A lighweight facade for the DynamoDB items table."""
+
+    name_ = "items"
+    primary_key = "id"
+
+    @staticmethod
+    def deserializer(item) -> Item:
+        return Item(
+            id=item["id"]["S"],
+            description=item["description"]["S"],
+        )
+
 
 app = FastAPI()
 
