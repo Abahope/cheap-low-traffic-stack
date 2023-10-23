f09d1a194ed4ef9f9d4bd850b698571eeb5a50e9
Nassir Al-Khishman <khishmann@gmail.com>
2023-10-22 22:23:39 -0400

    (frontend) Show current items

--- frontend/src/app/items/page.tsx
+++ frontend/src/app/items/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+import { apiHost } from "@/constants";
+import React from "react";
 
 export default function Items() {
+  // Get items
+  const [items, setItems] = React.useState<
+    Record<string, { id: string; description: string }>
+  >({});
+  React.useEffect(() => {
+    fetch(`${apiHost}/items`)
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <main>
       <h1>Items</h1>
+      <h2>Current items</h2>
+      <ul>
+        {Object.values(items).map((item) => (
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ul>
     </main>
   );
 }
