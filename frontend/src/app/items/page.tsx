3dfed2c6890adf955b22b387c6af559ee80a6bf6
Nassir Al-Khishman <khishmann@gmail.com>
2023-10-22 22:21:02 -0400

    (frontend) Add items page

---
+++ frontend/src/app/items/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Items() {
+  return (
+    <main>
+      <h1>Items</h1>
+    </main>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import LambdaWarmer from "./LambdaWarmer";
 
 export default function Home() {
   return (
     <main>
       <LambdaWarmer />
+      <Link href="/items">Items</Link>
       <h1>Home</h1>
       <p>
         Look here while the Lambda warms up or the items page might be slow!
